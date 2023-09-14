b094d61b3045c84eb353d5cdbdfc1f7577210e34
mateushilariodias <mateusilariodias@gmail.com>
2023-09-14 10:57:17 -0300

    use fastify

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 
-const server = createServer((request, response) => {
-    response.write('Listening');
+// const server = createServer((request, response) => {
+//     response.write('Oi');
 
-    return response.end()
+//     return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from 'fastify';
+
+const server = fastify();
+
+server.get('/', () => {
+    return "Hello, world!";
 })
 
-server.listen(3333)
+server.get('/mateus', () => {
+    return "Hello, Nateus!";
+})
+
+server.get('/sofia', () => {
+    return "Hello, Sofia!";
+})
+
+server.listen({
+    port: 3333,
+});
+
