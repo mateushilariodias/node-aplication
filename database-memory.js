7244c8948d39f0e03c374d9f660f303a4367bf58
mateushilariodias <mateusilariodias@gmail.com>
2023-09-17 07:50:51 -0300

    add routes.http  and change server.js and database-memory.js

--- database-memory.js
+++ database-memory.js
@@ -1,5 +1,23 @@
-export class DatabaseMemory {
-    #videos = [];
+import { randomUUID } from 'node:crypto'
 
-    
+export class DatabaseMemory {
+    #videos = new Map;
+
+    create(video) {
+        const videoId = randomUUID;
+        this.#videos.set(videoId, video);
+    }
+
+    update(id, video) {
+        this.#videos.set(id, video);
+    }
+
+    delete(id) {
+        this.#videos.delete(id);
+    }
+
+    list() {
+        return this.#videos.values();
+    }
+
 }
--- server.js
+++ server.js
@@ -12,9 +12,15 @@ import { fastify } from 'fastify';
 import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify();
+const database = new DatabaseMemory();
 
 server.post('/videos', () => {
-    return "Hello, post";
+    database.create({
+        title: "Video 01",
+        description: 'Esse é o video 01',
+        duration: 180,
+    });
+    console.log(database.list());
 })
 
 server.get('/videos', () => {
