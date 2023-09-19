086230b3f2bccaa6264552523465c0dcdc97e207
mateushilariodias <mateusilariodias@gmail.com>
2023-09-18 23:54:34 -0300

    change server.js and routes.http

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ import { DatabaseMemory } from './database-memory.js'
 const server = fastify();
 const database = new DatabaseMemory();
 
-server.post('/videos', () => {
+server.post('/videos', (request, reply) => {
     database.create({
         title: "Video 01",
         description: 'Esse é o video 01',
         duration: 180,
     });
-    console.log(database.list());
+
+    return reply.status(201).send();
 })
 
 server.get('/videos', () => {
